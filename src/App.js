08bcfc4d22a9e88fa31207c4e679c1cd824ec181
LeslieGG <ggarciaveka@gmail.com>
2025-07-27 14:34:55 -0500

    player working

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import "./App.css";
 import Footer from "./Components/Footer";
 import Button from "./Components/Button";
 import { ReactComponent as Play } from "./assets/Icons/play.svg";
+import { ReactComponent as Pause } from "./assets/Icons/pause.svg";
 import { ReactComponent as Backward } from "./assets/Icons/backward.svg";
 import { ReactComponent as Forward } from "./assets/Icons/forward.svg";
 import Song from "./Components/Song";
 import Loader from "./Components/Loader";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [songs, setSongs] = useState(null);
+  const [currentSong, setCurrentSong] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(true);
+  const audioRef = useRef(null);
+
   useEffect(() => {
     fetch("https://playground.4geeks.com/sound/songs")
       .then((response) => response.json())
@@ -24,6 +30,44 @@ function App() {
       });
   }, []);
 
+  const playSong = (index) => {
+    if (!songs.length || index < 1 || index >= songs.length) return;
+    const song = songs[index];
+    const songFullUrl = `https://playground.4geeks.com${song.url}`;
+    setCurrentIndex(index);
+    setCurrentSong(songFullUrl);
+
+    if (audioRef.current) {
+      audioRef.current.src = songFullUrl;
+      audioRef.current.play();
+    }
+  };
+
+  const togglePlay = () => {
+    if (!audioRef.current) return;
+    if (audioRef.current.paused) {
+      audioRef.current.play();
+      setIsPlaying(true);
+    } else {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  const nextSong = () => {
+    if (currentIndex == null) return;
+    let nextIndex = currentIndex + 1;
+    if (nextIndex >= songs.length) nextIndex = 0; //si se acabo el index entonces el siguiente reinicia en 0
+    playSong(nextIndex);
+  };
+
+  const prevSong = () => {
+    if (currentIndex == null) return;
+    let prevIndex = currentIndex - 1;
+    if (prevIndex < 0) prevIndex = songs.length - 1;
+    playSong(prevIndex);
+  };
+
   if (isLoading) {
     return <Loader />;
   }
@@ -31,20 +75,35 @@ function App() {
   return (
     <div className="App ">
       {songs.length &&
-        songs.map((song) => (
-          <Song key={song.id} num={song.id} name={song.name} /> //se asigna la key para que no tenga problemas de children lo ideal que sea ID
+        songs.map((song, index) => (
+          <>
+            <Song
+              key={song.id} //se asigna la key para que no tenga problemas de children lo ideal que sea ID
+              num={song.id}
+              name={song.name}
+              onClick={() => playSong(index)}
+              active={index === currentIndex}
+            />
+          </>
         ))}
+      <audio ref={audioRef} hidden />
       <Footer>
         <Button
-          onClick={() => console.log("back")}
+          onClick={() => prevSong()}
           icon={<Backward width={20} height={20} />}
         />
         <Button
-          onClick={() => console.log("play")}
-          icon={<Play width={20} height={20} />}
+          onClick={() => togglePlay()}
+          icon={
+            !isPlaying ? (
+              <Play width={20} height={20} />
+            ) : (
+              <Pause width={20} height={20} />
+            )
+          }
         />
         <Button
-          onClick={() => console.log("for")}
+          onClick={() => nextSong()}
           icon={<Forward width={20} height={20} />}
         />
       </Footer>
--- src/Components/Song/index.jsx
+++ src/Components/Song/index.jsx
@@ -1,7 +1,7 @@
 import "./songStyle.css";
-export default function Song({ num, name, onClick }) {
+export default function Song({ num, name, onClick, active }) {
   return (
-    <div onClick={onClick} className="song">
+    <div onClick={onClick} className={` ${active ? "active-song" : "song"}`}>
       <div className="d-flex">
         <div className="p-2 col-1 text-gray fw-bold">{num}</div>
         <div className="p-2 text-white justify-content-start">{name}</div>
